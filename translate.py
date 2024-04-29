f3e8893cce6eb91c1cd58a2a9d972bdcbc33e2a9
AndreaZecca <andreazecca03@gmail.com>
2024-04-29 11:27:22 +0200

    translation pipeline

--- translate.py
+++ translate.py
@@ -1,33 +1,61 @@
 import click
 from datasets import load_dataset
-from utils import get_translation_template
+from utils import get_translation_template, load_model, query_model
 import json
 
 def commonsense_qa(split):
     cqa = load_dataset('tau/commonsense_qa', split=split)
+    model, tokenizer = load_model()
     json_cqa = []
     for item in cqa:
         question = item['question'].strip()
         concept = item['question_concept']
         choices = list(zip(item['choices']['label'], item['choices']['text']))
         answer = item['answerKey'] if item['answerKey'].strip() != '' else 'N/A'
-        promtp_input = get_translation_template(question)
-        fol_question = '' # TODO: Implement LogicLlama's FOL translation
+        promtp_input = get_translation_template(dataset='commonsense_qa', question=question)
+        fol_question = query_model(question, dataset='commonsense_qa')
         json_cqa.append({
             'question': question,
             'concept': concept,
             'choices': [f'{label}) {text}' for label, text in choices],
             'answer': answer,
+            'fol_question': fol_question,
+            'prompt_input': promtp_input,
             'fol_question': fol_question
         })
-    with open(f'FOL/cqa.json', 'w') as f:
+    with open(f'FOL/cqa_{split}.json', 'w') as f:
         json.dump(json_cqa, f, indent=4)
 
 def hotpotqa(split):
     hqa = load_dataset('hotpot_qa', 'fullwiki', split=split)
+    model, tokenizer = load_model()
     json_hqa = []
     for item in hqa:
-        # TODO: How to deal with contexts
+        question = item['question'].strip()
+        concept = item['type'].strip()
+        answer = item['answer'].strip()
+        contexts = []
+        for supporing_title, supporting_id in zip(item['supporting_facts']['title'], item['supporting_facts']['sent_id']):
+            try:
+                sentences_index = item['context']['title'].index(supporing_title)
+                sentences = item['context']['sentences'][sentences_index]
+                supported_sentence = sentences[supporting_id]
+                contexts.append(supported_sentence)
+            except:
+                continue
+        promtp_input = get_translation_template(dataset='hotpotqa', question=question, context="\n".join(contexts))
+        fol_question = query_model(question, dataset='hotpotqa')
+        json_hqa.append({
+            'question': question,
+            'concept': concept,
+            'answer': answer,
+            'contexts': contexts,
+            'fol_question': fol_question,
+            'prompt_input': promtp_input
+        })
+    with open(f'FOL/hqa_{split}.json', 'w') as f:
+        json.dump(json_hqa, f, indent=4)
+
 
 @click.command()
 @click.option('--dataset', '-d', help='Dataset to be used', type=click.Choice(['commonsense_qa', 'hotpotqa']))
--- utils.py
+++ utils.py
@@ -1,6 +1,55 @@
 import os
+import torch
+from transformers import GenerationConfig, LlamaForCausalLM, LlamaTokenizer, LlamaForCausalLM
+from utils import DataPreparer, all_exists
+from peft import PeftModel, prepare_model_for_int8_training
+from typing import Dict, Optional, Callable
+from functools import partial
 
 def get_translation_template(dataset, **formatting_kwargs):
     base_template = open(f'./templates/translation/{dataset}.txt', 'r').read()
     template = base_template.format(**formatting_kwargs)
     return template
+
+def load_model():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    tokenizer = LlamaTokenizer.from_pretrained(base_model)
+    tokenizer.add_special_tokens({
+        "eos_token": "</s>",
+        "bos_token": "<s>",
+        "unk_token": '<unk>',
+        "pad_token": '<unk>',
+    })
+    tokenizer.padding_side = "left"  # Allow batched inference
+
+    model = LlamaForCausalLM.from_pretrained(
+        'meta-llama/Llama-2-7b',
+        load_in_8bit=load_in_8bit,
+        torch_dtype=torch.float16,
+        device_map='auto',
+    )
+
+    model = prepare_model_for_int8_training(model)
+
+    model = PeftModel.from_pretrained(
+        model,
+        'yuan-yang/LogicLLaMA-7b-direct-translate-delta-v0',
+        torch_dtype=torch.float16
+    )
+
+    model.to(device)
+
+    return model, tokenizer
+
+
+def query_model(input_prompt, dataset, model, tokenizer):
+    generation_config = GenerationConfig(
+        temperature=0.1,
+        top_p=0.75,
+        top_k=40,
+        num_beams=1
+    )
+    input_ids = tokenizer.encode(input_prompt, return_tensors='pt')
+    input_ids = input_ids.to(model.device)
+    output = model.generate(input_ids, **generation_config)
+    return tokenizer.decode(output[0], skip_special_tokens=True)
