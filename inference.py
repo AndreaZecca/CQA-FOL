9d6d447cba678a846c48c9c133f5091ecc804541
AndreaZecca <andreazecca03@gmail.com>
2024-05-13 11:47:38 +0200

    code to test on cqa20 and obqa

--- inference.py
+++ inference.py
@@ -27,7 +27,6 @@ def commonsense_qa(input_filename, output_filename, template_filename):
     with open(f'outputs/{output_filename}.json', 'w',  encoding='utf-8') as f:
         json.dump(cqa, f, indent=4, ensure_ascii=False)
 
-
 def hotpotqa(input_filename, output_filename, template_filename):
     with open(f'outputs/{input_filename}.json', 'r', encoding='utf-8') as f:
         hqa = json.load(f)
@@ -52,78 +51,73 @@ def hotpotqa(input_filename, output_filename, template_filename):
     with open(f'outputs/{output_filename}.json', 'w', encoding='utf-8') as f:
         json.dump(hqa, f, indent=4, ensure_ascii=False)
 
-
-# def commonsense_qa(input_filename, output_filename, template_filename, BATCH_SIZE=8):
-#     with open(f'outputs/{input_filename}.json', 'r') as f:
-#         cqa = json.load(f)
-#     model = load_vllm_inference_model()
-#     iterations = len(cqa) // BATCH_SIZE
-#     for i in tqdm(range(iterations + 1)):
-#         b_start = i * BATCH_SIZE
-#         b_end = min((i + 1) * BATCH_SIZE, len(cqa))
-#         batch = [cqa[j] for j in range(b_start, b_end)]
-
-#         batch_nl_questions = [item['nl_question'] for item in batch]
-#         batch_fol_questions = [item['fol_question'] for item in batch]
-
-#         batch_possible_answers = ['\n'.join(item['possible_answers']) for item in batch]
+def openbookqa(input_filename, output_filename, template_filename):
+    with open(f'outputs/{input_filename}.json', 'r', encoding='utf-8') as f:
+        obqa = json.load(f)
         
-#         batch_prompt_inputs = [
-#             get_template(task='inference', template_filename=template_filename, formatting_kwargs={
-#                 'fol_question': fol_question,
-#                 'possible_answers': possible_answers
-#             }) for fol_question, possible_answers in zip(batch_fol_questions, batch_possible_answers)
-#         ]
+    model, tokenizer = load_inference_model()
+    for i, item in enumerate(tqdm(obqa)):
+        nl_question = item['nl_question']
+        fol_question = item['fol_question']
 
-#         batch_model_outputs = query_vllm_model(model=model, input_prompts=batch_prompt_inputs, max_tokens=8)
-#         for j, item in enumerate(batch):
-#             item['model_output'] = batch_model_outputs[j]
+        nl_context = item['nl_context']
+        fol_context = item['fol_context']
 
-#     with open(f'outputs/{output_filename}.json', 'w',  encoding='utf-8') as f:
-#         json.dump(cqa, f, indent=4, ensure_ascii=False)
+        possible_answers = '\n'.join(item['possible_answers'])
 
+        input_prompt = get_template(task='inference', template_filename=template_filename, formatting_kwargs={
+            'fol_context': fol_context,
+            'fol_question': fol_question,
+            'possible_answers': possible_answers
+        })
 
+        model_output = query_model(tokenizer=tokenizer, model=model, input_prompt=input_prompt, max_tokens=8)
+        model_answer = model_output.split('### Answer')[-1].strip()
+        item['model_output'] = model_output
+        item['model_answer'] = model_answer
 
-# def hotpotqa(input_filename, output_filename, template_filename, BATCH_SIZE=8):
-#     with open(f'outputs/{input_filename}.json', 'r') as f:
-#         hqa = json.load(f)
-#     model = load_vllm_inference_model()
-#     iterations = len(hqa) // BATCH_SIZE
-#     for i in tqdm(range(iterations + 1)):
-#         b_start = i * BATCH_SIZE
-#         b_end = min((i + 1) * BATCH_SIZE, len(hqa))
-#         batch = [hqa[j] for j in range(b_start, b_end)]
+    with open(f'outputs/{output_filename}.json', 'w', encoding='utf-8') as f:
+        json.dump(obqa, f, indent=4, ensure_ascii=False)
 
-#         batch_nl_questions = [item['nl_question'] for item in batch]
-#         batch_fol_questions = [item['fol_question'] for item in batch]
+def cqa_2_0(input_filename, output_filename, template_filename):
+    with open(f'outputs/{input_filename}.json', 'r', encoding='utf-8') as f:
+        cqa_2_0 = json.load(f)
+    model, tokenizer = load_inference_model()
+    for i, item in enumerate(tqdm(cqa_2_0)):
+        nl_question = item['nl_question']
+        fol_question = item['fol_question']
 
-#         batch_nl_contexts = ['\n'.join(item['nl_contexts']) for item in batch]
-#         batch_fol_contexts = ['\n'.join(item['fol_contexts']) for item in batch]
+        correct_answer = item['correct_answer']
+        possible_answers = 'A) yes\nB) no'
 
-#         batch_prompt_inputs = [
-#             #TODO: Fix this
-#         ]
+        input_prompt = get_template(task='inference', template_filename=template_filename, formatting_kwargs={
+            'fol_question': nl_question,
+            'possible_answers': possible_answers,
+        })
 
-#         batch_model_outputs = query_vllm_model(model=model, input_prompts=batch_prompt_inputs, max_tokens=8)
+        model_output = query_model(tokenizer=tokenizer, model=model, input_prompt=input_prompt, max_tokens=8)
+        model_answer = model_output.split('### Answer')[-1].strip()
+        item['model_output'] = model_output
+        item['model_answer'] = model_answer
 
-#         for j, item in enumerate(batch):
-#             item['model_output'] = batch_model_outputs[j]
-
-#     with open(f'outputs/{output_filename}.json', 'w',  encoding='utf-8') as f:
-#         json.dump(hqa, f, indent=4, ensure_ascii=False)
-
+    with open(f'outputs/{output_filename}.json', 'w', encoding='utf-8') as f:
+        json.dump(cqa_2_0, f, indent=4, ensure_ascii=False)
 
 @click.command()
-@click.option('--dataset', '-d', help='Dataset to be used', type=click.Choice(['cqa', 'hqa']))
+@click.option('--dataset', '-d', help='Dataset to be used', type=click.Choice(['cqa', 'hqa', 'obqa', 'cqa_2_0']))
 @click.option('--input', '-i', type=str, help='Dataset to perform inference on')
 @click.option('--output', '-o', type=str, help='Output filename')
 @click.option('--template', '-t', type=str, help='Template filename for inference prompt')
-# @click.option('--batch_size', '-b', type=int, default=8, help='Batch size for inference')
 def inference(dataset, input, output, template):
-    if dataset == 'cqa':
-        commonsense_qa(input, output, template)
-    elif dataset == 'hqa':
-        hotpotqa(input, output, template)
+    callables = {
+        'cqa': commonsense_qa,
+        'hqa': hotpotqa,
+        'obqa': openbookqa,
+        'cqa_2_0': cqa_2_0
+    }
+
+    if dataset in callables:
+        callables[dataset](input, output, template)    
     else:
         raise ValueError('Dataset not found')
 
--- translate.py
+++ translate.py
@@ -1,6 +1,6 @@
 import click
 from datasets import load_dataset
-from utils import get_template, query_translation_model, load_vllm_translation_model, query_vllm_model
+from utils import get_template, load_vllm_translation_model, query_vllm_model
 import json
 from tqdm import tqdm
 
@@ -34,7 +34,6 @@ def commonsense_qa(split, template_filename, output_filename, BATCH_SIZE = 8):
             })            
     with open(f'outputs/{output_filename}.json', 'w', encoding='utf-8') as f:
         json.dump(json_cqa, f, indent=4, ensure_ascii=False)
-
 
 def hotpotqa(split, template_filename, output_filename, BATCH_SIZE = 8):
     hqa = load_dataset('hotpot_qa', 'fullwiki', split=split)
@@ -82,18 +81,91 @@ def hotpotqa(split, template_filename, output_filename, BATCH_SIZE = 8):
     with open(f'outputs/{output_filename}.json', 'w', encoding='utf-8') as f:
         json.dump(json_hqa, f, indent=4, ensure_ascii=False)
 
+def openbookqa(split, template_filename, output_filename, BATCH_SIZE = 8):
+    obqa = load_dataset('allenai/openbookqa', 'additional', split=split)
+    model = load_vllm_translation_model()
+    json_obqa = []
+    iterations = len(obqa) // BATCH_SIZE
+    for i in tqdm(range(iterations + 1)):
+        b_start = i * BATCH_SIZE
+        b_end = min((i + 1) * BATCH_SIZE, len(obqa))
+        batch = [obqa[j] for j in range(b_start, b_end)]
+
+        batch_questions = [item['question_stem'] for item in batch]
+        batch_choices = [list(zip(item['choices']['label'],item['choices']['text'])) for item in batch]
+        batch_possible_answers = [[f'{label}) {text}' for label, text in choices] for choices in batch_choices]
+        batch_correct_answers = [item['answerKey'] if item['answerKey'].strip() != '' else 'N/A' for item in batch]
+        batch_contexts = [item['fact1'] for item in batch]
+
+        questions_templates = [get_template(task='translation', template_filename=template_filename, formatting_kwargs={'question': question}) for question in batch_questions]
+        contexts_templates = [get_template(task='translation', template_filename=template_filename, formatting_kwargs={'question': context}) for context in batch_contexts]
+
+        prompt_inputs = [
+            *questions_templates,
+            *contexts_templates
+        ]
+
+        fol_results = query_vllm_model(model=model, input_prompts=prompt_inputs, max_tokens=512)
+
+        fol_questions = fol_results[:len(batch)]
+        fol_contexts = fol_results[len(batch):]
+
+        for j in range(len(batch)):
+            json_obqa.append({
+                'nl_question': batch_questions[j],
+                'nl_context': batch_contexts[j] if len(batch_contexts) > 0 else 'N/A',
+                'possible_answers': batch_possible_answers[j],
+                'correct_answer': batch_correct_answers[j],
+                'fol_question': fol_questions[j],
+                'fol_context': fol_contexts[j] if len(fol_contexts) > 0 else 'N/A'
+            })
+
+    with open(f'outputs/{output_filename}.json', 'w', encoding='utf-8') as f:
+        json.dump(json_obqa, f, indent=4, ensure_ascii=False)
+
+def cqa_2_0(split, template_filename, output_filename, BATCH_SIZE = 8):
+    cqa_2_0 = load_dataset('tasksource/commonsense_qa_2.0', split=split)
+    model = load_vllm_translation_model()
+    json_cqa_2_0 = []
+    iterations = len(cqa_2_0) // BATCH_SIZE
+    for i in tqdm(range(iterations+1)):
+        b_start = i * BATCH_SIZE
+        b_end = min((i + 1) * BATCH_SIZE, len(cqa_2_0))
+        batch = [cqa_2_0[j] for j in range(b_start, b_end)]
+
+        batch_questions = [item['question'] for item in batch]
+        batch_correct_answers = [item['answer'] if item['answer'].strip() != '' else 'N/A' for item in batch]
+
+        batch_prompt_inputs = [get_template(task='translation', template_filename=template_filename, formatting_kwargs={'question': question}) for question in batch_questions]
+
+        batch_fol_questions = query_vllm_model(model=model, input_prompts=batch_prompt_inputs)
+
+        for j in range(len(batch)):
+            json_cqa_2_0.append({
+                'nl_question': batch_questions[j],
+                'correct_answer': batch_correct_answers[j],
+                'fol_question': batch_fol_questions[j]
+            })
+
+    with open(f'outputs/{output_filename}.json', 'w', encoding='utf-8') as f:
+        json.dump(json_cqa_2_0, f, indent=4, ensure_ascii=False)
+
 
 @click.command()
-@click.option('--dataset', '-d', help='Dataset to be used', type=click.Choice(['cqa', 'hqa']))
+@click.option('--dataset', '-d', help='Dataset to be used', type=click.Choice(['cqa', 'hqa', 'obqa', 'cqa2']))
 @click.option('--split', '-s', help='Split to be used', type=click.Choice(['train', 'validation', 'test']), default='train')
 @click.option('--template', '-t', help='Template file name', required=True)
 @click.option('--output', '-o', help='Output file name', required=True)
 @click.option('--batch_size', '-b', help='Batch size', default=8, required=False, type=int)
 def translate(dataset, split, template, output, batch_size):
-    if dataset == 'cqa':
-        commonsense_qa(split, template, output, batch_size)
-    elif dataset == 'hqa':
-        hotpotqa(split, template, output, batch_size)
+    callables = {
+        'cqa': commonsense_qa,
+        'hqa': hotpotqa,
+        'obqa': openbookqa,
+        'cqa2': cqa_2_0
+    }
+    if dataset in callables:
+        callables[dataset](split, template, output, batch_size)    
     else:
         raise ValueError('Dataset not found')
 
